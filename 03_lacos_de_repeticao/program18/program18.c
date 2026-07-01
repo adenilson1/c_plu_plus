2741a44bf8474824a746dc932a0fdcfa8bc801fc
adenilson1 <adenilson99@hotmail.com>
2026-07-01 12:11:39 -0300

    Exercício de Fixação 3 - Conferir se um numero é primo

--- 03_lacos_de_repeticao/program18/program18.c
+++ 03_lacos_de_repeticao/program18/program18.c
@@ -1,24 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+#include<stdbool.h>
 
 int main(){
     int numero;
     int cont = 0;
+    bool verif_num = false;
 
     setlocale(LC_ALL, "portuguese");
 
     printf("Programa que analisa se um numero é primo ou nao : \n\n");
 
-    printf("\nDigite um numero interio : ");
-    scanf("%d", &numero);
 
-    for(int i = 2; i <= abs(numero); i++){
+    // Verificação se o numero é maior que 1
+    while(verif_num != true){
+         printf("\nDigite um numero interio : ");
+         scanf("%d", &numero);
+
+         if(numero < 2){
+            printf("\nVocê digitou um número menor ou igual 1\n");
+            printf("Tente Novamente\n");
+            verif_num = false;
+         }else{
+             verif_num = true;
+
+         }
+    }
+
+    // resto de divisão
+    for(int i = 2; i <= numero; i++){
         if(numero%i == 0){
-            cont ++;
+            cont++;
         }
     }
 
+    //Analise se o numero é primo
     if(cont == 1){
         printf("Numero primo\n");
     }else{
